7fa413e0afbe8c67d466ab7010869f46581c148f
Harish <bysaniharishguptha@gmail.com>
2024-09-18 17:42:55 +0530

    cors updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,14 +14,11 @@ connectDb();
 //middlewares.
 app.use(express.json());
 // Set up CORS
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "*", // Allow requests from this origin
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  credentials: true, // Allow credentials (cookies, authorization headers)
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*", // Allow requests from any origin
+  })
+);
 //routes
 app.use("/notes", require("../routes/notesRoutes.js"));
 app.use("/user", require("../routes/userRoutes.js"));
