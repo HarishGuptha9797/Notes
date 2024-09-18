92bcad3468c32aaafad3bda7939f800ea4cb2ab4
Harish <bysaniharishguptha@gmail.com>
2024-09-18 17:53:22 +0530

    updated dbconnection

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,15 +1,24 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  const connect = await mongoose
-    .connect(process.env.CONNECTION_STRING)
-    .then(() => {
-      console.log("database Connected");
-    })
-    .catch((e) => {
-      console.log(e.message);
-      process.exit(1);
+  const connectionString = process.env.CONNECTION_STRING;
+
+  if (!connectionString) {
+    console.error("CONNECTION_STRING environment variable is not set");
+    process.exit(1); // Exit the process with an error code
+  }
+
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log("Database Connected");
+  } catch (e) {
+    console.error("Error connecting to the database:", e.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDb;
